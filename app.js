cfbc00ea991465f4ffdef2314fbf9e8e2f547224
Tushar Shrivastava <tushariitb2@gmail.com>
2019-01-11 17:59:14 +0530

    making production ready

--- app.js
+++ app.js
@@ -49,4 +49,13 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// Serve static assets if in production mode
+if(process.env.NODE_ENV === "production"){
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 module.exports = app;
